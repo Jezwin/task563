4d0ecde85d355ec01dd06708036d560271d01ab0
Jezwin <jezwn@outlook.com>
2019-12-23 15:57:22 +0530

    cq:dialog and basic structure of every component

---
+++ core/src/main/java/com/auki/core/models/HowToJoinModel.java
@@ -0,0 +1,20 @@
+package com.auki.core.models;
+
+import javax.inject.Inject;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+
+@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class HowToJoinModel {
+	
+
+	@Inject
+	private Resource tiles;
+	
+	public Resource getTiles() {
+		return tiles;
+	}
+
+}
